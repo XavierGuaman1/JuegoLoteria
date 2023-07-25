b5b2766c51de5ab2ca67556eca89da6619ea62c2
carlosduti70 <cmzuniga@sudamericano.edu.ec>
2023-07-25 13:41:10 -0500

    condicional

--- index.py
+++ index.py
@@ -1,1 +1,14 @@
-print("hello word")
+lotery_ticket ={
+    'animal':'lion',
+    'winner_number':'6666'
+}
+
+boleto= input('ingresa tu boleto: ')
+
+
+if boleto in lotery_ticket['animal']=='lion' and lotery_ticket['winner_number']== '6666':
+    print('you winner')
+elif boleto in lotery_ticket['animal']== 'lion' and not lotery_ticket['winner_number']== 6666:
+    print('tienes otro chanse')
+else:
+    print('you lose')
